3218805a152977351dadad6ce95cb1cd3fbebd1f
Vinayak Vispute <93467074+VinayakVispute@users.noreply.github.com>
2022-11-21 21:26:26 +0530

    Intro to Packages

---
+++ Packages_Intro/app.py
@@ -0,0 +1,13 @@
+# this is normal method , Which is verbose
+"""import ecommerce.shipping
+
+
+ecommerce.shipping.calc_shipping() """
+
+from ecommerce.shipping import calc_shipping
+from ecommerce import shipping
+from ecommerce.shipping import calc_shipping,calc_total_shipping
+
+calc_total_shipping()
+
+calc_shipping()
---
+++ Packages_Intro/ecommerce/shipping.py
@@ -0,0 +1,6 @@
+def calc_shipping():
+    print("Shipping Calculated")
+
+
+def calc_total_shipping():
+    print("Shipping with Delivery Charges Printed")
