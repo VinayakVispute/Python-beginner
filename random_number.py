1dc9f076f8a97e6a47b1da071fc81a33a92f6782
Vinayak Vispute <93467074+VinayakVispute@users.noreply.github.com>
2022-11-21 21:24:14 +0530

    Random Numbers Generator

---
+++ random_number.py
@@ -0,0 +1,9 @@
+import random
+
+# Python code to print random numbers between 0 to 20
+for i in range(3):
+    print(random.randint(10,20))
+
+members=['Yash Sencho', 'Vinayak', 'Jayesh', 'Kartik', 'Nishit']
+gali_ka_bakra=random.choice(members)
+print(gali_ka_bakra)
